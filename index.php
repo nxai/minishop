c3e5eda88a80d4ae92b433ec46ec66662652a1b5
Somxai <nxai96@gmail.com>
2026-02-16 14:56:22 +0700

    add new files

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
-<?php include ("./includes/header.php"); ?>
-    <div class="container">
-        
+<?php include("./includes/header.php"); ?>
+<div class="container">
+    <div class="row">
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
-<?php include ("./includes/footer.php"); ?>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    </div>
+</div>
+<?php include("./includes/footer.php"); ?>
